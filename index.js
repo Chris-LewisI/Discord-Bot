e7892741ad37b8272b7fb6e6aacb82b9d90fe27d
Chris-LewisI <cli14@scarletmail.rutgers.edu>
2020-04-07 10:10:03 -0400

    Added uptime tracker

--- index.js
+++ index.js
@@ -108,3 +108,13 @@ client.on("message", message => {
         message.channel.send('🔫Modern Warfare Happy Hour starts at 9PM for the [KOLOTS]🔫');
     }
 });
+
+//KOFTA uptime
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+
+let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
