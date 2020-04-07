c0fd4fd6a2df8922da364995665c1b9e42281837
Chris-LewisI <cli14@scarletmail.rutgers.edu>
2020-04-07 10:38:45 -0400

    Valorant Announcement

--- index.js
+++ index.js
@@ -74,8 +74,17 @@ client.on("message", message => {
         }
     }
 
+    if(command === 'ping') {
+        message.channel.send('Pong! (~ ' + client.ping + 'ms)');
+    }
+
     if(command === 'uptime') {
-        message.channel.send(uptime);
+        let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
+  
+        message.channel.send(`__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
     }
     
     if (command === 'help') {
