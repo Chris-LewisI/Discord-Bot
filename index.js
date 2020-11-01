f42f53a6d57282ea561e830fe7117b73b88f782c
Chris Ibraheem <cli14@scarletmail.rutgers.edu>
2020-11-01 12:44:35 -0500

    Updated prefix, removed unused directories

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ client.on('message', message => {
       .setAuthor(client.user.username, 'attachment://kofta.png')
       .setTitle('Help is here!')
       .addFields(
-        { name: 'KOFTA Version:', value: `*${version}*\n**C O M M A N D S**\n- "//warzone" : Gives you access to Call Of Duty: Warzone patch notes 🍖\n- "//happy_hour" : Shows when COD Happy Hour begins for KOLOTS\n- "//info" : Displays KOFTA's uptime and ping!\n**U P D A T E S**\n- welcome and farewell message\n- role assignment upon joining server\n\n*Questions and recommendations can be DM'ed to the bot. Use the prefix "//" before your message!*`, inline: true })
+        { name: 'KOFTA Version:', value: `*${version}*\n**C O M M A N D S**\n- "$warzone" : Gives you access to Call Of Duty: Warzone patch notes 🍖\n- "$happy_hour" : Shows when COD Happy Hour begins for KOLOTS\n- "$info" : Displays KOFTA's uptime and ping!\n**U P D A T E S**\n- welcome and farewell message\n- role assignment upon joining server\n\n*Questions and recommendations can be DM'ed to the bot. Use the prefix "$" before your message!*`, inline: true })
 
     message.channel.send(embed)
     return
