450766a47f942d7c6ea408265d158928fe1400b8
Chris Lewis <chrisibraheem@gmail.com>
2025-01-21 12:27:46 -0500

    Removing useless files and adding package-lock.json to gitignore

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  fallout: path.join(__dirname, 'fallout.gif'),
-  kofta: path.join(__dirname, 'kofta.png'),
-  server: path.join(__dirname, 'server.gif'),
-  thumbUp: path.join(__dirname, 'thumbUp.png'),
-}
