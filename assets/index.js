96150e24e74397cac26396867d0fd7a100c690b4
Chris Lewis <chrisibraheem@gmail.com>
2025-01-22 13:34:57 -0500

    Fixing npm package version and asset export

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+  fallout: path.join(__dirname, 'fallout.gif'),
+  kofta: path.join(__dirname, 'kofta.png'),
+  server: path.join(__dirname, 'server.gif'),
+  thumbUp: path.join(__dirname, 'thumbUp.png'),
+}
